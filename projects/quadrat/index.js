a40cb075a906c530c770d96aeccbb448b4445141
Sergey <itsib.su@gmail.com>
2023-04-22 09:09:30 +0700

    Fixed quadrat adapter

--- projects/quadrat/index.js
+++ projects/quadrat/index.js
@@ -1,27 +1,33 @@
-const { GraphQLClient, gql } = require('graphql-request')
-const sdk = require('@defillama/sdk')
+const { GraphQLClient, gql } = require("graphql-request");
+const sdk = require("@defillama/sdk");
+const BigNumber = require('bignumber.js');
 
-const CHAINS = ['polygon', 'optimism', 'ethereum', 'arbitrum'];
+const CHAINS = ["polygon", "optimism", "ethereum", "arbitrum", "bsc"];
 
 const GRAPH_CLIENTS = {
-  polygon: new GraphQLClient('https://api.thegraph.com/subgraphs/name/krivochenko/plasma-quadrat-matic'),
-  optimism: new GraphQLClient('https://api.thegraph.com/subgraphs/name/krivochenko/plasma-quadrat-optimism'),
-  ethereum: new GraphQLClient('https://api.thegraph.com/subgraphs/name/krivochenko/plasma-quadrat-mainnet'),
-  arbitrum: new GraphQLClient('https://api.thegraph.com/subgraphs/name/krivochenko/plasma-quadrat-arbitrum'),
+  ethereum: new GraphQLClient("https://api.thegraph.com/subgraphs/name/ilyamk/quadrat-v1-ethereum"),
+  polygon: new GraphQLClient("https://api.thegraph.com/subgraphs/name/ilyamk/quadrat-v1-polygon"),
+  arbitrum: new GraphQLClient("https://api.thegraph.com/subgraphs/name/ilyamk/quadrat-v1-arbitrum"),
+  optimism: new GraphQLClient("https://api.thegraph.com/subgraphs/name/ilyamk/quadrat-v1-optimism"),
+  bsc: new GraphQLClient("https://api.thegraph.com/subgraphs/name/ilyamk/quadrat-v1-bnb")
 };
 
 const GRAPH_QUERY = gql`
     query strategies ($timestamp: Int! $skip: Int! $first: Int!) {
-        strategies (skip:$skip first:$first) {
-            token0 {
-                id
+        strategies(skip: $skip first: $first) {
+            meta {
+                token0 {
+                    id
+                    decimals
+                }
+                token1 {
+                    id
+                    decimals
+                }
             }
-            token1 {
-                id
-            }
-            daysData (orderBy: createdAt orderDirection: desc skip:0 first: 1,where: {createdAt_gte:$timestamp}) {
-                amount0
-                amount1
+            history (orderBy: timestamp orderDirection: desc skip:0 first: 1 where: { timestamp_gte: $timestamp timeframe: ONE_HOUR }) {
+                tvlToken0
+                tvlToken1
             }
         }
     }
@@ -29,33 +35,40 @@ const GRAPH_QUERY = gql`
 
 function chainTvlFactory(chain) {
   return async (timestamp) => {
+    timestamp = Math.floor(timestamp / 3600) * 3600;
     const graphQLClient = GRAPH_CLIENTS[chain];
     const result = await graphQLClient.request(GRAPH_QUERY, { timestamp, skip: 0, first: 1000 });
     if (!Array.isArray(result.strategies)) {
-      return {}
+      return {};
     }
 
-    const balances = {}
+
+    const balances = {};
+
     result.strategies.forEach(strategy => {
-      const dayData = strategy.daysData[0];
-      if (dayData)  {
-        sdk.util.sumSingleBalance(balances, strategy.token0.id, dayData.amount0);
-        sdk.util.sumSingleBalance(balances, strategy.token1.id, dayData.amount1);
+      const timeframe = strategy.history[0];
+      if (timeframe) {
+        const tvlToken0Raw = new BigNumber(timeframe.tvlToken0).times(new BigNumber(10).pow(+strategy.meta.token0.decimals)).toFixed(0, 1);
+        const tvlToken1Raw = new BigNumber(timeframe.tvlToken1).times(new BigNumber(10).pow(+strategy.meta.token1.decimals)).toFixed(0, 1);
+
+        sdk.util.sumSingleBalance(balances, strategy.meta.token0.id, tvlToken0Raw);
+        sdk.util.sumSingleBalance(balances, strategy.meta.token1.id, tvlToken1Raw);
       }
     });
-    return balances
+
+    return balances;
   };
 }
 
 module.exports = {
   timetravel: true,
   misrepresentedTokens: false,
-  methodology: 'Counts the tokens locked in Strategy Vaults in Uniswap v3 Pools.',
+  methodology: "Counts the tokens locked in Strategy Vaults in Uniswap v3 Pools.",
   start: 1667197843, // Mon Oct 31 2022 06:30:43 GMT+0000
   ...CHAINS.reduce((tvls, chain) => {
     tvls[chain] = {
-      tvl: chainTvlFactory(chain),
+      tvl: chainTvlFactory(chain)
     };
     return tvls;
-  }, {}),
-}
+  }, {})
+};
